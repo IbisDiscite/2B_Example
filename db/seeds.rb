bb0a96713e7ac53bf5b76ef98c0b4523b2423a68
David Felipe Rodriguez <dafrodriguezro@unal.edu.co>
2018-04-30 20:56:51 -0500

    Cambio a seed, solucionando errores

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,52 +5,52 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Unit.create(:nombre => "Grammar")
-Unit.create(:nombre => "Listening")
-Unit.create(:nombre => "Phrasal Verbs")
-Unit.create(:nombre => "Writing")
-Unit.create(:nombre => "Reading")
-Unit.create(:nombre => "Pronouns")
+Unit.create(id: 1, nombre: "Grammar")
+Unit.create(id: 2, nombre: "Listening")
+Unit.create(id: 3 , nombre: "Phrasal Verbs")
+Unit.create(id: 4 , nombre: "Writing")
+Unit.create(id: 5 , nombre: "Reading"
+Unit.create(id: 6 , nombre: "Pronouns")
 
 #Examples for grammar
 4.times do |i|
   Examples.create(
-    :unit_id => 1,
-    :contenido => "Example number #{i} of Grammar unit!"
+    unit_id: 1,
+    contenido: "Example number #{i} of Grammar unit!"
   )
 end
 #Examples for listening
 4.times do |i|
   Examples.create(
-    :unit_id => 2,
-    :contenido => "Example number #{i} of Listening unit!"
+    unit_id: 2,
+    contenido: "Example number #{i} of Listening unit!"
   )
 end
 #Examples for phrasal verbs
 4.times do |i|
   Examples.create(
-    :unit_id => 3,
-    :contenido => "Example number #{i} of Phrasal Verbs unit!"
+    unit_id: 3,
+    contenido: "Example number #{i} of Phrasal Verbs unit!"
   )
 end
 #Examples for writing
 4.times do |i|
   Examples.create(
-    :unit_id => 4,
-    :contenido => "Example number #{i} of Writing unit!"
+    unit_id: 4,
+    contenido: "Example number #{i} of Writing unit!"
   )
 end
 #Examples for reading
 4.times do |i|
   Examples.create(
-    :unit_id => 5,
-    :contenido => "Example number #{i} of Reading unit!"
+    unit_id: 5,
+    contenid "Example number #{i} of Reading unit!"
   )
 end
 #Examples for pronouns
 4.times do |i|
   Examples.create(
-    :unit_id => 6,
-    :contenido => "Example number #{i} of Pronouns unit!"
+    unit_id: 6,
+    contenido: "Example number #{i} of Pronouns unit!"
   )
 end
