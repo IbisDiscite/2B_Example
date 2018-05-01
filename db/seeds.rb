e7ea9abca2c657d5648396aefe44d4572ba26647
David Felipe Rodriguez <dafrodriguezro@unal.edu.co>
2018-05-01 13:39:09 -0500

    Añadida destruccion al seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Example.destroy_all
+Unit.destroy_all
+
 Unit.create(id: 1, nombre: "Grammar")
 Unit.create(id: 2, nombre: "Listening")
 Unit.create(id: 3 , nombre: "Phrasal Verbs")
