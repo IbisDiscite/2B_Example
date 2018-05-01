2e68cbb677e144f609cba55cd7e9126c6a39217c
David Felipe Rodriguez <dafrodriguezro@unal.edu.co>
2018-04-30 20:25:09 -0500

    Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,52 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Unit.create(:id=> 1, :nombre => "Grammar")
+Unit.create(:id=> 2, :nombre => "Listening")
+Unit.create(:id=> 3, :nombre => "Phrasal Verbs")
+Unit.create(:id=> 4, :nombre => "Writing")
+Unit.create(:id=> 5, :nombre => "Reading")
+Unit.create(:id=> 6, :nombre => "Pronouns")
+
+#Examples for grammar
+4.times do |i|
+  Examples.create(
+    :unit_id => 1,
+    :contenido => "Example number #{i} of Grammar unit!"
+  )
+end
+#Examples for listening
+4.times do |i|
+  Examples.create(
+    :unit_id => 2,
+    :contenido => "Example number #{i} of Listening unit!"
+  )
+end
+#Examples for phrasal verbs
+4.times do |i|
+  Examples.create(
+    :unit_id => 3,
+    :contenido => "Example number #{i} of Phrasal Verbs unit!"
+  )
+end
+#Examples for writing
+4.times do |i|
+  Examples.create(
+    :unit_id => 4,
+    :contenido => "Example number #{i} of Writing unit!"
+  )
+end
+#Examples for reading
+4.times do |i|
+  Examples.create(
+    :unit_id => 5,
+    :contenido => "Example number #{i} of Reading unit!"
+  )
+end
+#Examples for pronouns
+4.times do |i|
+  Examples.create(
+    :unit_id => 6,
+    :contenido => "Example number #{i} of Pronouns unit!"
+  )
+end
